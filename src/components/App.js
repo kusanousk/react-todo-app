8d4a66efea64ac9de11891fc93ef9eb0a87aaa2f
kusanousk <kusanousk@gmail.com>
2021-11-18 03:04:15 +0900

    TODO一覧を状態別に表示

--- src/components/App.js
+++ src/components/App.js
@@ -15,8 +15,13 @@ function App() {
     fetchData();
   }, []);
 
-  // FIXME 後で削除
-  console.log("TODOリスト", todoList);
+  const inCompletedList = todoList.filter((todo) => {
+    return !todo.done;
+  });
+
+  const completedList = todoList.filter((todo) => {
+    return todo.done;
+  });
 
   return (
     <>
@@ -26,11 +31,24 @@ function App() {
 
       <button>+ TODOを追加</button>
 
-      <h2>TODOリスト</h2>
+      <h2>未完了TODOリスト</h2>
       <ul>
-        {todoList.map((todo) => (
+        {inCompletedList.map((todo) => (
           <li key={todo.id}>
-            {todo.content}({todo.done ? "完了" : "未完了"})
+            {todo.content}
+            <button>({todo.done ? "未完了リストへ" : "完了リストへ"})</button>
+            <button>削除</button>
+          </li>
+        ))}
+      </ul>
+
+      <h2>完了TODOリスト</h2>
+      <ul>
+        {completedList.map((todo) => (
+          <li key={todo.id}>
+            {todo.content}
+            <button>({todo.done ? "未完了リストへ" : "完了リストへ"})</button>
+            <button>削除</button>
           </li>
         ))}
       </ul>
